4cbffc6f315be076a7a2fb2fa3e78e1970835a39
ThomasGorisse <thomas.gorisse@gmail.com>
2022-01-03 15:41:48 +0100

    Call setDisplayGeometry after Session is resumed

--- arsceneview/src/main/kotlin/io/github/sceneview/ar/ArSceneView.kt
+++ arsceneview/src/main/kotlin/io/github/sceneview/ar/ArSceneView.kt
@@ -109,19 +109,6 @@ open class ArSceneView @JvmOverloads constructor(
     override fun onArSessionCreated(session: ArSession) {
         super.onArSessionCreated(session)
 
-        // Don't remove this code-block. It is important to correctly
-        // set the DisplayGeometry for the ArCore-Session if four
-        // example the permission Dialog is shown on the screen.
-        // If we remove this part, the camera is flickering if returned
-        // from the permission Dialog.
-        if (renderer.desiredWidth != 0 && renderer.desiredHeight != 0) {
-            session.setDisplayGeometry(
-                display!!.rotation,
-                renderer.desiredWidth,
-                renderer.desiredHeight
-            )
-        }
-
         // Feature config, therefore facing direction, can only be configured once per session.
         if (session.cameraConfig.facingDirection == FacingDirection.FRONT) {
             renderer.isFrontFaceWindingInverted = true
@@ -129,6 +116,21 @@ open class ArSceneView @JvmOverloads constructor(
 
         // Set max frames per seconds here.
         maxFramesPerSeconds = session.cameraConfig.fpsRange.upper
+    }
+
+    override fun onArSessionResumed(session: ArSession) {
+        super.onArSessionResumed(session)
+
+        // Don't remove this code-block. It is important to correctly set the DisplayGeometry for
+        // the ArCore-Session if for example the permission Dialog is shown on the screen.
+        // If we remove this part, the camera is flickering if returned from the permission Dialog.
+        if (renderer.desiredWidth != 0 && renderer.desiredHeight != 0) {
+            session.setDisplayGeometry(
+                display!!.rotation,
+                renderer.desiredWidth,
+                renderer.desiredHeight
+            )
+        }
     }
 
     override fun onArSessionConfigChanged(session: ArSession, config: Config) {
