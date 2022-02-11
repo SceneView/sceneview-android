cc67d928e358a64dd76b8ae4256d9e3dede46e83
ThomasGorisse <thomas.gorisse@gmail.com>
2022-02-11 16:37:33 +0100

    New HitTestSystem including Depth and InstantPlacement

--- arsceneview/src/main/kotlin/io/github/sceneview/ar/arcore/ArFrame.kt
+++ arsceneview/src/main/kotlin/io/github/sceneview/ar/arcore/ArFrame.kt
@@ -16,12 +16,9 @@ data class ArFrame(
 
     // TODO : Make a quick test with androidCameraTimestamp
     val timestamp get() = frame.timestamp
+    var updatedTrackables: List<Trackable> =
+        frame.getUpdatedTrackables(Trackable::class.java).toList()
 
-    var updatedTrackables: List<Trackable> = listOf()
-
-    init {
-        updatedTrackables = frame.getUpdatedTrackables(Trackable::class.java).toList()
-    }
 
     /**
      * ### Performs a ray cast to retrieve the hit trackables
@@ -30,21 +27,35 @@ data class ArFrame(
      * camera view. Intersections with detected scene geometry are returned, sorted by distance from
      * the device; the nearest intersection is returned first.
      *
-     * Note: Significant geometric leeway is given when returning hit results. For example, a plane
-     * hit may be generated if the ray came close, but did not actually hit within the plane extents
-     * or plane bounds [Plane.isPoseInExtents][com.google.ar.core.Plane.isPoseInExtents] and
+     * When using:
+     * - **Plane and/or Depth:** Significant geometric leeway is given when returning hit results.
+     * For example, a plane hit may be generated if the ray came close, but did not actually hit
+     * within the plane extents or plane bounds
+     * [Plane.isPoseInExtents][com.google.ar.core.Plane.isPoseInExtents] and
      * [Plane.isPoseInPolygon][com.google.ar.core.Plane.isPoseInPolygon] can be used to determine
      * these cases). A point (point cloud) hit is generated when a point is roughly within one
      * finger-width of the provided screen coordinates.
      *
-     * Note: When using
-     * [Session.Feature#FRONT_CAMERA][com.google.ar.core.Session.FeatureFRONT_CAMERA], the returned
-     * hit result list will always be empty, as the camera is not
+     * - **Instant Placement:** Ray cast can return a result before ARCore establishes full
+     * tracking. The pose and apparent scale of attached objects depends on the
+     * [InstantPlacementPoint][com.google.ar.core.InstantPlacementPoint] tracking method and the
+     * provided approximateDistanceMeters. A discussion of the different tracking methods and the
+     * effects of apparent object scale are described in
+     * [InstantPlacementPoint][com.google.ar.core.InstantPlacementPoint].
+     * This function will succeed only if
+     * [Config.InstantPlacementMode][com.google.ar.core.Config.InstantPlacementMode] is
+     * [com.google.ar.core.Config.InstantPlacementMode.LOCAL_Y_UP] in the ARCore session
+     * configuration, the ARCore session tracking state is
+     * [TrackingState.TRACKING][com.google.ar.core.TrackingState.TRACKING] }, and there are
+     * sufficient feature points to track the point in screen space.
+     *
+     * - **[Session.Feature.FRONT_CAMERA][com.google.ar.core.Session.Feature.FRONT_CAMERA]:**
+     * The returned hit result list will always be empty, as the camera is not
      * [TrackingState.TRACKING][com.google.ar.core.TrackingState.TRACKING]. Hit testing against
      * tracked faces is not currently supported.
      *
      * Note: In ARCore 1.24.0 or later on supported devices, if depth is enabled by calling
-     * [com.google.ar.core.ConfigsetDepthMode] with the value
+     * [com.google.ar.core.Config.setDepthMode] with the value
      * [Config.DepthMode.AUTOMATIC][com.google.ar.core.Config.DepthMode.AUTOMATIC], the returned
      * list includes [com.google.ar.core.DepthPoint] values sampled from the latest computed depth
      * image.
@@ -53,80 +64,69 @@ data class ArFrame(
      * *Default: The view width center*
      * @param yPx y view coordinate in pixelsls
      * *Default: The view height center*
-     *
-     * @return an ordered list of intersections with scene geometry, nearest hit first
-     */
-    @JvmOverloads
-    fun hitTests(
-        xPx: Float = session.displayWidth / 2.0f,
-        yPx: Float = session.displayHeight / 2.0f,
-    ): List<HitResult> = when {
-        frame.camera.trackingState == TrackingState.TRACKING -> {
-            frame.hitTest(xPx, yPx)
-        }
-        else -> listOf()
-    }
-
-    /**
-     * ### Performs a ray cast that can return a result before ARCore establishes full tracking
-     *
-     * The pose and apparent scale of attached objects depends on the
-     * [InstantPlacementPoint][com.google.ar.core.InstantPlacementPoint] tracking method and the
-     * provided approximateDistanceMeters. A discussion of the different tracking methods and the
-     * effects of apparent object scale are described in
-     * [InstantPlacementPoint][com.google.ar.core.InstantPlacementPoint].
-     *
-     * This function will succeed only if
-     * [Config.InstantPlacementMode][com.google.ar.core.Config.InstantPlacementMode] is
-     * [com.google.ar.core.Config.InstantPlacementMode.LOCAL_Y_UP] in the ARCore session
-     * configuration, the ARCore session tracking state is
-     * [TrackingState.TRACKING][com.google.ar.core.TrackingState.TRACKING] }, and there are
-     * sufficient feature points to track the point in screen space.
-     *
-     * @param xPx x screen coordinate in pixels
-     * *Default: The view width center*
-     * @param yPx y screen coordinate in pixels
-     * *Default: The view height center*
      * @param approximateDistanceMeters the distance at which to create an
      * [InstantPlacementPoint][com.google.ar.core.InstantPlacementPoint]. This is only used while
      * the tracking method for the returned point is
      * [InstantPlacementPoint.TrackingMethod.SCREENSPACE_WITH_APPROXIMATE_DISTANCE]
      * [com.google.ar.core.InstantPlacementPoint.TrackingMethod.SCREENSPACE_WITH_APPROXIMATE_DISTANCE]
-     * *Default: [io.github.sceneview.ar.ARCore][approximateDistanceMeters]*
+     * *Default: [ArSession.approximateDistanceMeters]*
+     * @param plane enable plane results
+     * *Default: [ArSession.planeFindingEnabled]*
+     * @param depth enable depth results
+     * *Default: [ArSession.depthEnabled]*
+     * @param instantPlacement enable instant placement results
+     * *Default: [ArSession.instantPlacementEnabled]*
      *
-     * @return if successful a list containing a single [HitResult][com.google.ar.core.HitResult],
-     * otherwise an empty list. The [HitResult][com.google.ar.core.HitResult] will have a trackable
-     * of type [InstantPlacementPoint][com.google.ar.core.InstantPlacementPoint].
+     * @return an ordered list of intersections with scene geometry, nearest hit first
+     * In case of instant placement result, if successful a list containing a single
+     * [HitResult][com.google.ar.core.HitResult], otherwise an empty list.
+     * The [HitResult][com.google.ar.core.HitResult] will have a trackable
+     * of type [InstantPlacementPoint][com.google.ar.core.InstantPlacementPoint]
      */
     @JvmOverloads
-    fun hitTestsInstantPlacement(
+    fun hitTests(
         xPx: Float = session.displayWidth / 2.0f,
         yPx: Float = session.displayHeight / 2.0f,
-        approximateDistanceMeters: Float = session.approximateDistanceMeters
-    ): List<HitResult> = when {
-        session.instantPlacementEnabled -> {
-            frame.hitTestInstantPlacement(xPx, yPx, approximateDistanceMeters)
+        approximateDistanceMeters: Float = session.approximateDistanceMeters,
+        plane: Boolean = session.planeFindingEnabled,
+        depth: Boolean = session.depthEnabled,
+        instantPlacement: Boolean = session.instantPlacementEnabled
+    ): List<HitResult> {
+        if(camera.isTracking) {
+            if (plane || depth) {
+                frame.hitTest(xPx, yPx).takeIf { it.isNotEmpty() }?.let {
+                    return it
+                }
+            }
+            if (instantPlacement) {
+                return frame.hitTestInstantPlacement(xPx, yPx, approximateDistanceMeters)
+            }
         }
-        else -> listOf()
+        return listOf()
     }
 
     /**
      * @see hitTests
+     * @see firstValid
      */
     @JvmOverloads
-    fun hitTests(
-        motionEvent: MotionEvent
-    ): Collection<HitResult> = hitTests(motionEvent.x, motionEvent.y)
-
-    /**
-     * @see hitTestsInstantPlacement
-     */
-    @JvmOverloads
-    fun hitTestsInstantPlacement(
-        motionEvent: MotionEvent,
-        approximateDistanceMeters: Float = session.approximateDistanceMeters
-    ): Collection<HitResult> =
-        hitTestsInstantPlacement(motionEvent.x, motionEvent.y, approximateDistanceMeters)
+    fun hitTest(
+        xPx: Float = session.displayWidth / 2.0f,
+        yPx: Float = session.displayHeight / 2.0f,
+        approximateDistanceMeters: Float = session.approximateDistanceMeters,
+        plane: Boolean = session.planeFindingEnabled,
+        depth: Boolean = session.depthEnabled,
+        instantPlacement: Boolean = session.instantPlacementEnabled
+    ): HitResult? {
+        return hitTests(
+            xPx = xPx,
+            yPx = yPx,
+            approximateDistanceMeters = approximateDistanceMeters,
+            plane = plane,
+            depth = depth,
+            instantPlacement = instantPlacement
+        ).firstValid(camera)
+    }
 
     /**
      * @see hitTests
@@ -134,39 +134,19 @@ data class ArFrame(
      */
     @JvmOverloads
     fun hitTest(
-        xPx: Float,
-        yPx: Float
-    ): HitResult? = hitTests(xPx, yPx).firstValid(camera)
-
-    /**
-     * @see hitTestsInstantPlacement
-     * @see firstValid
-     */
-    @JvmOverloads
-    fun hitTestInstantPlacement(
-        xPx: Float,
-        yPx: Float,
-        approximateDistanceMeters: Float = session.approximateDistanceMeters
-    ): HitResult? = hitTestsInstantPlacement(xPx, yPx, approximateDistanceMeters).firstValid(camera)
-
-    /**
-     * @see hitTests
-     * @see firstValid
-     */
-    @JvmOverloads
-    fun hitTest(
-        motionEvent: MotionEvent
-    ): HitResult? = hitTests(motionEvent).firstValid(frame.camera)
-
-    /**
-     * @see hitTestsInstantPlacement
-     * @see firstValid
-     */
-    @JvmOverloads
-    fun hitTestInstantPlacement(
         motionEvent: MotionEvent,
-        approximateDistanceMeters: Float = session.approximateDistanceMeters
-    ): HitResult? = hitTestsInstantPlacement(motionEvent).firstValid(frame.camera)
+        approximateDistanceMeters: Float = session.approximateDistanceMeters,
+        plane: Boolean = session.planeFindingEnabled,
+        depth: Boolean = session.depthEnabled,
+        instantPlacement: Boolean = session.instantPlacementEnabled
+    ): HitResult? = hitTests(
+        xPx = motionEvent.x,
+        yPx = motionEvent.y,
+        approximateDistanceMeters = approximateDistanceMeters,
+        plane = plane,
+        depth = depth,
+        instantPlacement = instantPlacement
+    ).firstValid(frame.camera)
 
     /**
      * ### Creates a new anchor at the hit location.
@@ -179,13 +159,21 @@ data class ArFrame(
      * hitResult.getTrackable().createAnchor(hitResult.getHitPose())}
      *
      * @param xPx x view coordinate in pixels
-     * The view width center is the default value
+     * *Default: The view width center*
      * @param yPx y view coordinate in pixelsls
-     * The view height center is the default value
-     * @param approximateDistanceMeters only if Instant Placement is enabled
-     * The distance at which to create an [InstantPlacementPoint].
-     * This is only used while the tracking method for the returned point is
+     * *Default: The view height center*
+     * @param approximateDistanceMeters the distance at which to create an
+     * [InstantPlacementPoint][com.google.ar.core.InstantPlacementPoint]. This is only used while
+     * the tracking method for the returned point is
      * [InstantPlacementPoint.TrackingMethod.SCREENSPACE_WITH_APPROXIMATE_DISTANCE]
+     * [com.google.ar.core.InstantPlacementPoint.TrackingMethod.SCREENSPACE_WITH_APPROXIMATE_DISTANCE]
+     * *Default: [ArSession.approximateDistanceMeters]*
+     * @param plane enable plane results
+     * *Default: [ArSession.planeFindingEnabled]*
+     * @param depth enable depth results
+     * *Default: [ArSession.depthEnabled]*
+     * @param instantPlacement enable instant placement results
+     * *Default: [ArSession.instantPlacementEnabled]*
      *
      * @see hitTests
      * @see #getHitPose()
@@ -193,8 +181,18 @@ data class ArFrame(
     fun createAnchor(
         xPx: Float = session.displayWidth / 2.0f,
         yPx: Float = session.displayHeight / 2.0f,
-        approximateDistanceMeters: Float = session.approximateDistanceMeters
-    ): Anchor? = hitTest(xPx, yPx)?.createAnchor()
+        approximateDistanceMeters: Float = session.approximateDistanceMeters,
+        plane: Boolean = session.planeFindingEnabled,
+        depth: Boolean = session.depthEnabled,
+        instantPlacement: Boolean = session.instantPlacementEnabled
+    ): Anchor? = hitTest(
+        xPx = xPx,
+        yPx = yPx,
+        approximateDistanceMeters = approximateDistanceMeters,
+        plane = plane,
+        depth = depth,
+        instantPlacement = instantPlacement
+    )?.createAnchor()
 
     /**
      * ### Retrieve the frame tracked Planes
@@ -259,14 +257,15 @@ data class ArFrame(
  * Hits are sorted by depth. Consider only closest hit on a plane, Oriented Point, Depth Point,
  * or Instant Placement Point.
  */
-fun Collection<HitResult>.firstValid(camera: Camera) = firstOrNull { hitResult ->
-    when (val trackable = hitResult.trackable!!) {
-        is Plane -> trackable.isPoseInPolygon(hitResult.hitPose) &&
-                hitResult.hitPose.calculateDistanceToPlane(camera.pose) > 0
-        // DepthPoints are only returned if Config.DepthMode is set to AUTOMATIC.
-        is DepthPoint -> true
-        is Point -> trackable.orientationMode == Point.OrientationMode.ESTIMATED_SURFACE_NORMAL
-        is InstantPlacementPoint -> true
-        else -> false
+fun Collection<HitResult>.firstValid(camera: Camera): HitResult? {
+    return firstOrNull { hitResult ->
+        when (val trackable = hitResult.trackable!!) {
+            is Plane -> trackable.isPoseInPolygon(hitResult.hitPose) &&
+                    hitResult.hitPose.calculateDistanceToPlane(camera.pose) > 0.0f
+            is DepthPoint -> trackable.isTracking
+            is Point -> trackable.orientationMode == Point.OrientationMode.ESTIMATED_SURFACE_NORMAL
+            is InstantPlacementPoint -> true
+            else -> false
+        }
     }
 }
