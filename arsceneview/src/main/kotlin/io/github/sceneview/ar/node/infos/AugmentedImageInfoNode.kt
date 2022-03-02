7395728261f6ff80925960813247bd077f239125
ThomasGorisse <thomas.gorisse@gmail.com>
2022-03-02 11:56:41 +0100

    Fix AugmentedImageInfoNode alignment

--- arsceneview/src/main/kotlin/io/github/sceneview/ar/node/infos/AugmentedImageInfoNode.kt
+++ arsceneview/src/main/kotlin/io/github/sceneview/ar/node/infos/AugmentedImageInfoNode.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.view.View
 import androidx.lifecycle.LifecycleCoroutineScope
 import com.google.ar.sceneform.rendering.RenderableInstance
+import com.google.ar.sceneform.rendering.ViewRenderable
 import io.github.sceneview.ar.R
 import io.github.sceneview.node.ViewNode
 import io.github.sceneview.math.Position
@@ -14,7 +15,10 @@ open class AugmentedImageInfoNode(context: Context, coroutineScope: LifecycleCor
     init {
         isFocusable = false
         position = Position(x = 0.0f, y = 0.0f, z = -2.0f)
-        loadView(context, R.layout.sceneview_view_info_augmented_image, coroutineScope)
+        loadView(context, R.layout.sceneview_view_info_augmented_image, coroutineScope) { _, _->
+            renderable?.horizontalAlignment = ViewRenderable.HorizontalAlignment.CENTER
+            renderable?.verticalAlignment = ViewRenderable.VerticalAlignment.CENTER
+        }
     }
 
     override fun onViewLoaded(renderableInstance: RenderableInstance, view: View) {
