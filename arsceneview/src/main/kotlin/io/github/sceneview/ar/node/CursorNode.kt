5a23dc8b8c7ed5134d8483279974cb5a945df47c
ThomasGorisse <thomas.gorisse@gmail.com>
2022-03-02 11:57:12 +0100

    Fix CursorNode default placement mode

--- arsceneview/src/main/kotlin/io/github/sceneview/ar/node/CursorNode.kt
+++ arsceneview/src/main/kotlin/io/github/sceneview/ar/node/CursorNode.kt
@@ -21,7 +21,7 @@ open class CursorNode(
     context: Context,
     coroutineScope: LifecycleCoroutineScope? = null,
     glbFileLocation: String = "sceneview/models/cursor.glb"
-) : ArModelNode() {
+) : ArModelNode(placementMode = PlacementMode.BEST_AVAILABLE) {
 
     var validColor: Color = Color(1.0f, 1.0f, 1.0f)
     var invalidColor: Color = Color(1.0f, 0.0f, 0.0f)
