749b458e0c531ffacb7d52c4a27e9a15ccd796fc
ThomasGorisse <thomas.gorisse@gmail.com>
2022-03-03 12:22:53 +0100

    Add RenderableInstance object culling setter

--- sceneview/src/main/java/com/google/ar/sceneform/rendering/RenderableInstance.java
+++ sceneview/src/main/java/com/google/ar/sceneform/rendering/RenderableInstance.java
@@ -294,6 +294,42 @@ public class RenderableInstance implements AnimatableModel {
     }
 
     /**
+     * ### Changes whether or not frustum culling is on
+     *
+     * The view frustum is the region of space in the modeled world that may appear on the screen.
+     * Viewing-frustum culling is the process of removing objects that lie completely outside the
+     * viewing frustum from the rendering process.
+     * In other words, `true` = your model won't be visible/rendered when not any part of its
+     * bounding box is visible/inside the camera view.
+     *
+     * Rendering these object would be a waste of time since they are not directly visible.
+     * To make culling fast, it is usually done using bounding box surrounding the objects rather
+     * than the objects themselves.
+     * Instead of sending all information to your GPU, you will sort visible and invisible elements
+     * and render only visible elements.
+     * Thanks to this technique, you will earn GPU compute time.
+     *
+     * Do not confuse frustum culling with backface culling. The latter is controlled via the
+     * material
+     *
+     * true by default
+     */
+    public void setCulling(boolean isCulling) {
+        RenderableManager renderableManager = EngineInstance.getEngine().getRenderableManager();
+        @EntityInstance int renderableInstance = renderableManager.getInstance(getEntity());
+        if (renderableInstance != 0 && renderableManager.hasComponent(renderableInstance)) {
+            renderableManager.setCulling(renderableInstance, isShadowCaster);
+        }
+        int[] entities = getFilamentAsset().getEntities();
+        for (int i = 0; i < entities.length; i++) {
+            renderableInstance = renderableManager.getInstance(entities[i]);
+            if (renderableInstance != 0) {
+                renderableManager.setCulling(renderableInstance, isCulling);
+            }
+        }
+    }
+
+    /**
      * Returns true if configured to cast shadows on other renderables.
      */
     public boolean isShadowCaster() {
