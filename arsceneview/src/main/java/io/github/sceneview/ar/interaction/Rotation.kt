fe24ade53d91c1c3829c1c55f407f2fbd140aaf9
Steven Mohr <97528052+StevenMOG@users.noreply.github.com>
2022-03-18 08:49:46 +0100

    Update arsceneview/src/main/kotlin/io/github/sceneview/ar/interaction/Rotation.kt
    Co-authored-by: Thomas Gorisse <thomas.gorisse@gmail.com>

--- arsceneview/src/main/java/io/github/sceneview/ar/interaction/Rotation.kt
+++ arsceneview/src/main/java/io/github/sceneview/ar/interaction/Rotation.kt
@@ -6,7 +6,7 @@ import dev.romainguy.kotlin.math.Quaternion
 import io.github.sceneview.ar.node.ArNode
 
 class RotationGesture(arNode: ArNode) : GestureStrategy(arNode) {
-    override fun rotate(rotateDelta: Float) {
+override fun rotate(delta: Float) {
         val rotationDelta = Quaternion.fromAxisAngle(Float3(0f, 1f, 0f), rotateDelta * 2.5f)
 arNode.modelQuaternion = arNode.modelQuaternion * rotationDelta
     }
