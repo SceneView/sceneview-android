eb5a52cc02e115b218cd67c840b66ac86e82a39a
Steven Mohr <steven.mohr@smartasapps.de>
2022-03-22 15:30:05 +0100

    Simplify translation

--- arsceneview/src/main/java/io/github/sceneview/ar/interaction/Translation.kt
+++ arsceneview/src/main/java/io/github/sceneview/ar/interaction/Translation.kt
@@ -1,78 +1,40 @@
 package io.github.sceneview.ar.interaction
 
 import com.google.ar.core.HitResult
-import com.google.ar.core.Plane
-import com.google.ar.core.TrackingState
+import io.github.sceneview.ar.ArSceneView
+import io.github.sceneview.ar.arcore.depthEnabled
+import io.github.sceneview.ar.arcore.instantPlacementEnabled
 import io.github.sceneview.ar.arcore.isTracking
-import io.github.sceneview.ar.node.ArModelNode
-import io.github.sceneview.ar.node.PlacementMode
+import io.github.sceneview.ar.arcore.planeFindingEnabled
+import io.github.sceneview.ar.node.ArNode
 
-internal class TranslationGesture(private var arModelNode: ArModelNode) :
-    GestureStrategy(arModelNode) {
+internal class TranslationGesture(arNode: ArNode) : GestureStrategy(arNode) {
     private var lastArHitResult: HitResult? = null
-
-    private val allowedPlaneTypes: Set<Plane.Type> = when (arNode.placementMode) {
-        PlacementMode.PLANE_HORIZONTAL -> setOf(
-            Plane.Type.HORIZONTAL_UPWARD_FACING,
-            Plane.Type.HORIZONTAL_DOWNWARD_FACING
-        )
-        PlacementMode.PLANE_VERTICAL -> setOf(Plane.Type.VERTICAL)
-        PlacementMode.PLANE_HORIZONTAL_AND_VERTICAL -> Plane.Type.values().toSet()
-        else -> emptySet()
-    }
 
     override fun beginGesture(x: Int, y: Int) {
         arNode.detachAnchor()
     }
 
     override fun continueGesture(x: Int, y: Int) {
- val arFrame = (arNode.getSceneViewInternal() as? ArSceneView)?.currentFrame ?: return
+        val sceneView = arNode.getSceneViewInternal() as? ArSceneView ?: return
+        val config = sceneView.arSessionConfig ?: return
+        val arFrame = sceneView.currentFrame ?: return
         arFrame.hitTest(
             xPx = x.toFloat(), yPx = y.toFloat(),
-            plane = placementMode.planeEnabled,
-            depth = placementMode.depthEnabled,
-            instantPlacement = placementMode.instantPlacementEnabled
+            plane = config.planeFindingEnabled,
+            depth = config.depthEnabled,
+            instantPlacement = config.instantPlacementEnabled
         )?.takeIf { it.isTracking }?.let { hitResult ->
-            lastHitResult = hitResult
+            lastArHitResult = hitResult
             hitResult.hitPose?.let { hitPose ->
                 arNode.pose = hitPose
             }
         }
-            }
     }
 
     override fun endGesture() {
-        val hitResult = lastArHitResult ?: return
-        if (hitResult.trackable.trackingState == TrackingState.TRACKING) {
-            val anchorNode = arNode
-            val oldAnchor = anchorNode.anchor
-            oldAnchor?.detach()
-            val newAnchor = hitResult.createAnchor()
-            anchorNode.anchor = newAnchor
-
-// TODO: View if it is usefull
-//      Vector3 worldPosition = getTransformableNode().getWorldPosition();
-//      Quaternion worldRotation = getTransformableNode().getWorldRotation();
-//      Quaternion finalDesiredWorldRotation = worldRotation;
-//
-//      // Since we change the anchor, we need to update the initialForwardInLocal into the new
-//      // coordinate space. Local variable for nullness analysis.
-//      Quaternion desiredLocalRotation = this.desiredLocalRotation;
-//      if (desiredLocalRotation != null) {
-//        getTransformableNode().setQuaternion(desiredLocalRotation);
-//        finalDesiredWorldRotation = getTransformableNode().getWorldRotation();
-//      }
-//
-//      anchorNode.setAnchor(newAnchor);
-//
-//      // Temporarily set the node to the final world rotation so that we can accurately
-//      // determine the initialForwardInLocal in the new coordinate space.
-//      getTransformableNode().setWorldRotation(finalDesiredWorldRotation);
-//      Vector3 initialForwardInWorld = getTransformableNode().getForward();
-//      initialForwardInLocal.set(anchorNode.worldToLocalDirection(initialForwardInWorld));
-//
-//      getTransformableNode().setWorldRotation(worldRotation);
-//      getTransformableNode().setWorldPosition(worldPosition);
+        lastArHitResult?.let { hitResult ->
+            arNode.anchor = hitResult.createAnchor()
         }
     }
 }
