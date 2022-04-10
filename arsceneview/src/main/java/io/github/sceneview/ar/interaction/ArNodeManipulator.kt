e4c8ca71db814b2fcdf5e86f4a84f633ca65e1ff
ThomasGorisse <thomas.gorisse@gmail.com>
2022-04-10 23:46:57 +0200

    Fix and cleanup ArNodeManipulator (partially)

--- arsceneview/src/main/java/io/github/sceneview/ar/interaction/ArNodeManipulator.kt
+++ arsceneview/src/main/java/io/github/sceneview/ar/interaction/ArNodeManipulator.kt
@@ -1,124 +1,98 @@
 package io.github.sceneview.ar.interaction
 
-import com.google.ar.core.HitResult
-import com.google.ar.core.TrackingState
 import dev.romainguy.kotlin.math.*
 import io.github.sceneview.ar.ArSceneView
 import io.github.sceneview.ar.arcore.*
 import io.github.sceneview.ar.node.ArNode
 import io.github.sceneview.ar.node.EditableTransform
+import io.github.sceneview.interaction.SelectedNodeVisualizer
 import io.github.sceneview.node.Node
-import io.github.sceneview.scene.SelectedNodeVisualizer
 
 open class ArNodeManipulator(
     protected val sceneView: ArSceneView
 ) {
-    var currentNode: ArNode? = null
-    private var currentGesture: EditableTransform? = null
+    var minZoomScale = 0.1f
+    var maxZoomScale = 10.0f
 
-    protected open val selectedNodeVisualizer: SelectedNodeVisualizer
+    private var currentGestureTransform: EditableTransform? = null
+
+    var selectedNode: ArNode? = null
+        set(value) {
+            if (field != value) {
+                field?.let { selectedNodeVisualizer.selectNode(it, false) }
+                value?.let { selectedNodeVisualizer.selectNode(it, true) }
+                field = value
+            }
+        }
+
+    open val selectedNodeVisualizer: SelectedNodeVisualizer
         get() = sceneView.selectedNodeVisualizer
 
     open fun onNodeTouch(node: Node) {
-        val oldCurrentNode = currentNode
-        currentNode = node as? ArNode
-        if (oldCurrentNode == currentNode) return
-        currentNode?.let { selectedNodeVisualizer.applySelectionVisual(it) }
-        oldCurrentNode?.let { selectedNodeVisualizer.removeSelectionVisual(it) }
+        selectedNode = node as? ArNode
     }
 
-    open fun beginRotate(): Boolean {
-        if (!(currentNode?.rotationEditable == true && currentGesture == null)) return false
-        currentGesture = EditableTransform.ROTATION
-        return true
-    }
+    open fun beginTransform(): Boolean =
+        selectedNode?.takeIf {
+            currentGestureTransform == EditableTransform.POSITION && it.positionEditable
+        }?.let { node ->
+            node.detachAnchor()
+        } != null
 
-    open fun rotate(deltaDegree: Float): Boolean =
-        runIfCurrentGestureIs(EditableTransform.ROTATION) {
-            val nodeToRotate =
-                currentNode?.takeIf { it.rotationEditable } ?: return@runIfCurrentGestureIs false
-            val rotationDelta =
-                normalize(Quaternion.fromAxisAngle(Float3(0f, 1f, 0f), degrees(deltaDegree)))
-            nodeToRotate.modelQuaternion = nodeToRotate.modelQuaternion * rotationDelta
-            true
-        }
-
-    open fun endRotate() = runIfCurrentGestureIs(EditableTransform.ROTATION) {
-        currentGesture = null
-        true
-    }
-
-    open fun beginScale(): Boolean {
-        if (!(currentNode?.scaleEditable == true && currentGesture == null)) return false
-        currentGesture = EditableTransform.SCALE
-        return true
-    }
-
-    open fun scale(factor: Float) = runIfCurrentGestureIs(EditableTransform.SCALE) {
-        val nodeToScale =
-            currentNode?.takeIf { it.scaleEditable } ?: return@runIfCurrentGestureIs false
-        nodeToScale.scale = clamp(
-            nodeToScale.scale * factor, 0.5f, 1.5f
-        )
-        true
-    }
-
-    open fun endScale() = runIfCurrentGestureIs(EditableTransform.SCALE) {
-        currentGesture = null
-        true
-    }
-
-    protected var lastArHitResult: HitResult? = null
-
-    val positionIsEditable: Boolean
-        get() = currentNode?.positionEditable ?: false
-
-    open fun beginTransform(): Boolean {
-        if (!(currentGesture == null && currentNode?.positionEditable == true)) return false
-        currentGesture = EditableTransform.POSITION
-        lastArHitResult = null
-        val nodeToTransform = currentNode?.takeIf { it.positionEditable } ?: return false
-        nodeToTransform.detachAnchor()
-        return true
-    }
-
-    open fun continueTransform(x: Float, y: Float) =
-        runIfCurrentGestureIs(EditableTransform.POSITION) {
-            val nodeToTransform =
-                currentNode?.takeIf { it.positionEditable } ?: return@runIfCurrentGestureIs false
-            val sceneView = nodeToTransform.getSceneViewInternal() as? ArSceneView
-                ?: return@runIfCurrentGestureIs false
-            val config = sceneView.arSessionConfig ?: return@runIfCurrentGestureIs false
-            val arFrame = sceneView.currentFrame ?: return@runIfCurrentGestureIs false
-            arFrame.hitTest(
-                xPx = x, yPx = y,
-                plane = config.planeFindingEnabled,
-                depth = config.depthEnabled,
-                instantPlacement = config.instantPlacementEnabled
-            )?.takeIf { it.isTracking }?.let { hitResult ->
-                lastArHitResult = hitResult
+    open fun continueTransform(x: Float, y: Float) : Boolean =
+        selectedNode?.takeIf {
+            currentGestureTransform == EditableTransform.POSITION && it.positionEditable
+        }?.let { node ->
+            node.hitTest(xPx = x, yPx = y)?.takeIf { it.isTracking }?.let { hitResult ->
                 hitResult.hitPose?.let { hitPose ->
-                    nodeToTransform.transform(hitPose.position, quaternion = hitPose.quaternion)
+                    node.pose = hitPose
                 }
             }
-            true
+        } != null
+
+    open fun endTransform() : Boolean =
+        selectedNode?.takeIf {
+            currentGestureTransform == EditableTransform.POSITION && it.positionEditable
+        }?.let { node ->
+            node.anchor()
+            currentGestureTransform = null
+        } != null
+
+    open fun beginRotate(): Boolean =
+        (currentGestureTransform == null && selectedNode?.rotationEditable == true).also {
+            currentGestureTransform = EditableTransform.ROTATION
         }
 
-    open fun endTransform() = runIfCurrentGestureIs(EditableTransform.POSITION) {
-        val nodeToTransform =
-            currentNode?.takeIf { it.positionEditable } ?: return@runIfCurrentGestureIs false
-        lastArHitResult?.takeIf { it.trackable.trackingState == TrackingState.TRACKING }
-            ?.let { hitResult ->
-                nodeToTransform.anchor = hitResult.createAnchor()
-            }
-        currentGesture = null
-        true
-    }
+    open fun rotate(deltaRadians: Float) : Boolean =
+        selectedNode?.takeIf {
+            currentGestureTransform == EditableTransform.ROTATION && it.rotationEditable
+        }?.let { node ->
+            // TODO: Fix a round rotation behavior
+            val rotationDelta = Quaternion.fromAxisAngle(Float3(y = 1.0f), degrees(deltaRadians))
+            node.modelQuaternion = node.modelQuaternion * rotationDelta
+        } != null
 
-    private fun runIfCurrentGestureIs(gesture: EditableTransform, block: () -> Boolean): Boolean {
-        if (currentGesture != gesture) return false
-        return block.invoke()
-    }
+    open fun endRotate(): Boolean =
+        (currentGestureTransform == EditableTransform.ROTATION &&
+            selectedNode?.rotationEditable == true)
+        .also { currentGestureTransform = null }
+
+    open fun beginScale(): Boolean =
+        (currentGestureTransform == null && selectedNode?.scaleEditable == true).also {
+            currentGestureTransform = EditableTransform.SCALE
+        }
+
+    open fun scale(factor: Float) : Boolean =
+        selectedNode?.takeIf {
+            currentGestureTransform == EditableTransform.SCALE && it.scaleEditable
+        }?.let { node ->
+            node.modelScale = clamp(node.modelScale * factor, minZoomScale, maxZoomScale)
+        } != null
+
+    open fun endScale(): Boolean =
+        (currentGestureTransform == EditableTransform.SCALE &&
+                selectedNode?.scaleEditable == true)
+            .also { currentGestureTransform = null }
 }
 
 internal val ArNode.positionEditable: Boolean
