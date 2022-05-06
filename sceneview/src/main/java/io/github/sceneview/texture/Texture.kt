3ad435fdc0f2578e9b37bb0778182d45d1a50a31
ThomasGorisse <thomas.gorisse@gmail.com>
2022-05-06 23:11:29 +0200

    ArCameraStream kotlined, cleaned, resource consumption safe

--- sceneview/src/main/java/io/github/sceneview/texture/Texture.kt
+++ sceneview/src/main/java/io/github/sceneview/texture/Texture.kt
@@ -1,5 +1,7 @@
 package io.github.sceneview.texture
 
+import androidx.annotation.IntRange
+import androidx.annotation.Size
 import androidx.lifecycle.Lifecycle
 import com.google.android.filament.Stream
 import com.google.android.filament.Texture
@@ -18,6 +20,12 @@ fun Texture.Builder.build(lifecycle: Lifecycle? = null): Texture = build(Filamen
 fun Texture.setExternalStream(stream: Stream) = setExternalStream(Filament.engine, stream)
 fun Texture.setImage(level: Int, buffer: Texture.PixelBufferDescriptor) =
     setImage(Filament.engine, level, buffer)
+
+fun Texture.setImage(
+    @IntRange(from = 0) level: Int,
+    buffer: Texture.PixelBufferDescriptor,
+    @Size(min = 6) faceOffsetsInBytes: IntArray
+) = setImage(Filament.engine, level, buffer, faceOffsetsInBytes)
 
 /**
  * Destroys a Texture and frees all its associated resources.
