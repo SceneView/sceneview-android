8e2ed2a36a1b5bde4fef07415bb2284f5a52a17f
ThomasGorisse <thomas.gorisse@gmail.com>
2022-06-22 19:44:57 +0200

    ArModelNode : Change namings for better comprehension + Add followHitPosition flag + Add maxHitTestPerSecond + Fixes + Doc

--- arsceneview/src/main/java/io/github/sceneview/ar/node/ArModelNode.kt
+++ arsceneview/src/main/java/io/github/sceneview/ar/node/ArModelNode.kt
@@ -5,7 +5,6 @@ import androidx.lifecycle.Lifecycle
 import com.google.ar.core.Anchor
 import com.google.ar.core.HitResult
 import com.google.ar.sceneform.rendering.RenderableInstance
-import dev.romainguy.kotlin.math.Float3
 import io.github.sceneview.SceneView
 import io.github.sceneview.ar.ArSceneView
 import io.github.sceneview.ar.arcore.ArFrame
@@ -29,22 +28,19 @@ import kotlin.math.abs
 open class ArModelNode : ArNode {
 
     /**
-     * ### The node camera/screen position
+     * ### The node camera/screen/view position where the hit will be made to find an AR position
      *
-     * - While there is no AR tracking information available, the node is following the camera moves
-     * so it stays at this camera/screen relative position [com.google.ar.sceneform.Camera] node is
-     * considered as the parent
-     * - ARCore will try to find the real world position of this screen position and the node
-     * [io.github.sceneview.node.Node.worldPosition] will be updated so.
+     * The Node will try to find the real world position/orientation of this screen coordinate and
+     * and place himself accordingly.
      *
-     * The Z value is only used when no surface is actually detected or when instant placement is
-     * enabled:
-     * - In case of instant placement disabled, the z position will be estimated by the AR surface
-     * distance at the (x,y) so this value is not used.
-     * - In case of instant placement enabled, this value is used as
-     * [approximateDistanceMeters][ArFrame.hitTest] to help ARCore positioning result.
+     * The Z value is only used when no surface is actually detected or when [followHitPosition] and
+     * [instantAnchor] is set to false or when instant placement is enabled:
+     * - In case of instant placement disabled, the z position (distance from the camera) will be
+     * estimated by the AR surface distance at the (x,y) so this value is not used.
+     * - In case of instant placement enabled, this value is used as an approximateDistanceMeters
+     * at the [ArFrame.hitTest] call to help ARCore positioning the result quickly.
      *
-     * By default, the node is positioned at the center screen, 2 meters forward
+     * By default, the node is positioned at the center screen, 4 meters forward
      *
      * **Horizontal (X):**
      * - left: x < 0.0f
@@ -74,15 +70,47 @@ open class ArModelNode : ArNode {
      * ----/----|---------
      *
      * +z ---- -y --------
+     *
+     * @see followHitPosition
      */
-    var placementPosition: Position = DEFAULT_PLACEMENT_POSITION
+    var hitPosition: Position = DEFAULT_HIT_POSITION
         set(value) {
             field = value
-            position = Float3(value)
+            position = Position(value)
         }
 
     /**
-     * ### Anchor the node as soon as an AR position/rotation is found
+     * ### Make the node follow the camera/screen matching real world positions
+     *
+     * The node [position] is updated with the realtime ARCore [pose] at the corresponding
+     * [hitPosition] until it is anchored ([isAnchored]) or until this this value is set to false.
+     *
+     * - While there is no AR tracking information available, the node is following the camera moves
+     * so it stays at this camera/screen relative position but without adjusting its position and
+     * orientation to the real world
+     *
+     * - Then ARCore will try to find the real world position of the node at the [hitPosition] by
+     * looking at its [hitTest] on each [onArFrame].
+     *
+     * - In case of instant placement disabled, the z position (distance from the camera) will be
+     * estimated by the AR surface distance at the (x,y).
+     *
+     * - The node rotation will be also adjusted in case of [PlacementMode.DEPTH] or depending on
+     * the detected planes orientations in case of [PlacementMode.PLANE_HORIZONTAL],
+     * [PlacementMode.PLANE_VERTICAL], [PlacementMode.PLANE_HORIZONTAL_AND_VERTICAL]
+     *
+     * HitTest might be consuming. Disable it if your don't need to retrieve the position on each
+     * frame on reduce the [maxHitTestPerSecond]
+     *
+     * Disable it if you manage the positioning by yourself or other AR methods (Augmented Images,
+     * Augmented Faces,...)
+     *
+     * @see hitPosition
+     */
+    var followHitPosition: Boolean = true
+
+    /**
+     * ### Anchor the node as soon as an AR position/rotation is found/available
      *
      * - `true` The node will be anchored in the real world at the first suitable place available.
      * Depending on your need, you can change the [placementMode] to adjust between a quick
@@ -91,9 +119,9 @@ open class ArModelNode : ArNode {
      * [PlacementMode.PLANE_HORIZONTAL_AND_VERTICAL]) or auto refining accuracy
      * ([PlacementMode.BEST_AVAILABLE]) placement.
      * - `false` The node will follow its AR [pose] and update it constantly to follow the
-     * camera [placementPosition] on the real world.
+     * camera [hitPosition] on the real world.
      */
-    var autoAnchor = false
+    var instantAnchor = false
         set(value) {
             field = value
             if (value) {
@@ -105,35 +133,58 @@ open class ArModelNode : ArNode {
             }
         }
 
+    /**
+     * ### Adjust the max screen [ArFrame.hitTest] number per seconds
+     *
+     * When this node is not anchored and [followHitPosition] is set to true, the node will
+     * constantly try to find its place in real world. Following the [hitPosition] on the
+     * screen.
+     *
+     * Decrease if you don't need a very precise position update and want to reduce frame
+     * consumption.
+     * Increase for a more accurate positioning update.
+     */
+    var maxHitTestPerSecond: Int = 10
+
     override var editableTransforms = setOf(EditableTransform.ROTATION, EditableTransform.SCALE)
 
-    var poseUpdatePrecision = 1.0f
+    var arFrame: ArFrame? = null
+        private set
+    var hitTestFrame: ArFrame? = null
+        private set
 
-    var lastArFrame: ArFrame? = null
-    var lastPoseUpdateFrame: ArFrame? = null
-    var lastHitResult: HitResult? = null
+    var hitResult: HitResult? = null
+        private set(value) {
+            field = value
+            if (followHitPosition &&
+                // Keep the last position when no tracking result
+                value?.isTracking == true
+            ) {
+                pose = value.hitPose
+            }
+            onHitResult(value)
+        }
 
-    /**
-     * TODO : Doc
-     */
-    var onArFrameHitResult: ((node: ArNode, hitResult: HitResult?, isTracking: Boolean) -> Unit)? =
-        null
+    var onHitResult: ((node: ArNode, hitResult: HitResult?) -> Unit)? = null
 
     /**
      * ### Construct a new placement ArModelNode
      *
      * @param placementMode See [ArModelNode.placementMode]
-     * @param autoAnchor See [ArModelNode.autoAnchor]
-     * @param placementPosition See [ArModelNode.placementPosition]
+     * @param hitPosition See [ArModelNode.hitPosition]
+     * @param followHitPosition See [ArModelNode.followHitPosition]
+     * @param instantAnchor See [ArModelNode.instantAnchor]
      */
     constructor(
         placementMode: PlacementMode = DEFAULT_PLACEMENT_MODE,
-        autoAnchor: Boolean = false,
-        placementPosition: Position = DEFAULT_PLACEMENT_POSITION
+        hitPosition: Position = DEFAULT_HIT_POSITION,
+        followHitPosition: Boolean = true,
+        instantAnchor: Boolean = false
     ) : super() {
         this.placementMode = placementMode
-        this.placementPosition = placementPosition
-        this.autoAnchor = autoAnchor
+        this.hitPosition = hitPosition
+        this.followHitPosition = followHitPosition
+        this.instantAnchor = instantAnchor
     }
 
     /**
@@ -193,25 +244,25 @@ open class ArModelNode : ArNode {
         super.onArFrame(arFrame)
 
         if (!isAnchored) {
-            if (!autoAnchor || !anchor()) {
-                if (arFrame.precision(lastPoseUpdateFrame) <= poseUpdatePrecision) {
-                    lastPoseUpdateFrame = arFrame
-                    onArFrameHitResult(hitTest(arFrame))
-                }
+            // Try to find an anchor if none ha been founded
+            if (instantAnchor) {
+                anchor()
+            } else if (followHitPosition &&
+                // Add condition in here if a new feature needs hit result
+                arFrame.fps(hitTestFrame) <= maxHitTestPerSecond
+            ) {
+                hitResult = hitTest(arFrame)
+                hitTestFrame = arFrame
             }
         }
-        lastArFrame = arFrame
+        this.arFrame = arFrame
     }
 
-    open fun onArFrameHitResult(hitResult: HitResult?) {
-        // Keep the last position when no tracking result
-        if (hitResult?.isTracking == true) {
-            lastHitResult = hitResult
-            hitResult.hitPose?.let { hitPose ->
-                pose = hitPose
-            }
-        }
-        onArFrameHitResult?.invoke(this, hitResult, isTracking)
+    /**
+     * TODO : Doc
+     */
+    open fun onHitResult(hitResult: HitResult?) {
+        onHitResult?.invoke(this, hitResult)
     }
 
     /**
@@ -220,9 +271,9 @@ open class ArModelNode : ArNode {
      * @param frame the [ArFrame] from where we take the [HitResult]
      * By default the latest session frame if any exist
      * @param xPx x view coordinate in pixels
-     * By default the [cameraPosition.x][placementPosition] of this Node is used
+     * By default the [cameraPosition.x][hitPosition] of this Node is used
      * @property yPx y view coordinate in pixels
-     * By default the [cameraPosition.y][placementPosition] of this Node is used
+     * By default the [cameraPosition.y][hitPosition] of this Node is used
      *
      * @return the hitResult or null if no info is retrieved
      *
@@ -230,14 +281,14 @@ open class ArModelNode : ArNode {
      */
     fun hitTest(
         frame: ArFrame? = arSession?.currentFrame,
-        approximateDistanceMeters: Float = abs(placementPosition.z),
+        approximateDistanceMeters: Float = abs(hitPosition.z),
         plane: Boolean = placementMode.planeEnabled,
         depth: Boolean = placementMode.depthEnabled,
         instantPlacement: Boolean = placementMode.instantPlacementEnabled
     ): HitResult? = super.hitTest(
         frame = frame,
-        xPx = (arSession?.displayWidth ?: 0) / 2.0f * (1.0f + placementPosition.x),
-        yPx = (arSession?.displayHeight ?: 0) / 2.0f * (1.0f - placementPosition.y),
+        xPx = (arSession?.displayWidth ?: 0) / 2.0f * (1.0f + hitPosition.x),
+        yPx = (arSession?.displayHeight ?: 0) / 2.0f * (1.0f - hitPosition.y),
         approximateDistanceMeters = approximateDistanceMeters,
         plane = plane,
         depth = depth,
@@ -246,7 +297,8 @@ open class ArModelNode : ArNode {
 
     override fun createAnchor(): Anchor? {
         return (hitTest()?.takeIf { it.isTracking }
-            ?: lastHitResult?.takeIf { it.isTracking })
+        // Fallback to last hit result
+            ?: hitResult?.takeIf { it.isTracking })
             ?.let { createAnchor(it) }
     }
 
@@ -259,11 +311,11 @@ open class ArModelNode : ArNode {
     fun copy(toNode: ArModelNode = ArModelNode()) = toNode.apply {
         super.copy(toNode)
 
-        placementPosition = this@ArModelNode.placementPosition
+        hitPosition = this@ArModelNode.hitPosition
     }
 
     companion object {
-        val DEFAULT_PLACEMENT_POSITION =
-            Position(0.0f, 0.0f, -DEFAULT_PLACEMENT_MODE.instantPlacementDistance)
+        val DEFAULT_HIT_POSITION =
+            Position(0.0f, 0.0f, -DEFAULT_PLACEMENT_DISTANCE)
     }
 }
