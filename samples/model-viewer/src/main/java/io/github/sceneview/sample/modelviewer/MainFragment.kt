961d6a8a5ef8d4658deb49aec93b376d179e5c94
ThomasGorisse <thomas.gorisse@gmail.com>
2022-07-08 19:24:31 +0200

    Renew the 3D ModelViewer sample

--- samples/model-viewer/src/main/java/io/github/sceneview/sample/modelviewer/MainFragment.kt
+++ samples/model-viewer/src/main/java/io/github/sceneview/sample/modelviewer/MainFragment.kt
@@ -6,8 +6,10 @@ import androidx.core.view.isGone
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import com.google.android.filament.utils.HDRLoader
+import dev.romainguy.kotlin.math.lookAt
 import io.github.sceneview.SceneView
 import io.github.sceneview.environment.loadEnvironment
+import io.github.sceneview.math.Direction
 import io.github.sceneview.math.Position
 import io.github.sceneview.math.Rotation
 import io.github.sceneview.node.ModelNode
@@ -31,11 +33,20 @@ class MainFragment : Fragment(R.layout.fragment_main) {
         loadingView = view.findViewById(R.id.loadingView)
 
         isLoading = true
-        sceneView.camera.position = Position(x = 4.0f, y = -1.0f)
-        sceneView.camera.rotation = Rotation(x = 0.0f, y = 80.0f)
 
-        val modelNode = ModelNode(position = Position(z = -4.0f))
+        val modelNode = ModelNode(
+            position = Position(z = -4.0f),
+            rotation = Rotation(x = 35.0f)
+        )
         sceneView.addChild(modelNode)
+
+        sceneView.camera.transform = lookAt(
+            eye = modelNode.worldPosition.let {
+                Position(x = it.x, y = it.y + 0.5f, z = it.z + 2.0f)
+            },
+            target = modelNode.worldPosition,
+            up = Direction(y = 1.0f)
+        )
 
         lifecycleScope.launchWhenCreated {
             sceneView.environment = HDRLoader.loadEnvironment(
@@ -43,23 +54,28 @@ class MainFragment : Fragment(R.layout.fragment_main) {
                 lifecycle = lifecycle,
                 hdrFileLocation = "environments/studio_small_09_2k.hdr",
                 specularFilter = false
-            )
+            )?.apply {
+                indirectLight?.intensity = 50_000f
+            }
 
             modelNode.loadModel(
                 context = requireContext(),
                 lifecycle = lifecycle,
                 glbFileLocation = "https://sceneview.github.io/assets/models/MaterialSuite.glb",
-                autoAnimate = true,
-                autoScale = true,
+                scaleToUnits = 1.0f,
                 centerOrigin = Position(x = 0.0f, y = 0.0f, z = 0.0f)
             )
-            // We currently have an issue while the model render is not completely loaded
-            delay(200)
+            delay(1500)
             isLoading = false
             sceneView.camera.smooth(
-                position = Position(x = -1.0f, y = 1.5f, z = -3.5f),
-                rotation = Rotation(x = -60.0f, y = -50.0f),
-                speed = 0.5f
+                lookAt(
+                    eye = modelNode.worldPosition.let {
+                        Position(x = it.x - 0.4f, y = it.y + 0.4f, z = it.z - 0.6f)
+                    },
+                    target = modelNode.worldPosition,
+                    up = Direction(y = 1.0f)
+                ),
+                speed = 0.7f
             )
         }
     }
