27f040814d13d459606e5a7ba8553cb87ee914b0
ThomasGorisse <thomas.gorisse@gmail.com>
2022-08-17 18:41:04 +0200

    Added model animator extensions

--- sceneview/src/main/java/io/github/sceneview/model/Model.kt
+++ sceneview/src/main/java/io/github/sceneview/model/Model.kt
@@ -1,5 +1,6 @@
 package io.github.sceneview.model
 
+import com.google.android.filament.gltfio.Animator
 import com.google.android.filament.gltfio.FilamentAsset
 import io.github.sceneview.Filament
 
@@ -7,19 +8,8 @@ typealias Model = FilamentAsset
 
 val Model.allEntities get() = entities + lightEntities
 
-val Model.animationCount: Int get() = animator.animationCount
-val Model.animationName: String get() = animationName(0)
-val Model.animationDuration: Float get() = animationDuration(0)
-
-fun Model.animationName(animationIndex: Int): String =
-    animator.getAnimationName(animationIndex)
-
-fun Model.animationDuration(animationIndex: Int): Float =
-    animator.getAnimationDuration(animationIndex)
-
-fun Model.applyAnimation(animationIndex: Int, time: Float) =
-    animator.applyAnimation(animationIndex, time)
-
+fun Animator.getAnimationIndex(animationName: String) =
+    (0 until animationCount).firstOrNull { getAnimationName(it) == animationName }
 
 fun Model.destroy() {
     releaseSourceData()
