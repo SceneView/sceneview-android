9f2c7779b18741ccbe862d6615c4b33ea565180a
ThomasGorisse <thomas.gorisse@gmail.com>
2022-11-25 20:18:55 +0100

    New ModelNode features and architecture

--- sceneview/src/main/java/io/github/sceneview/components/ModelChildComponent.kt
+++
@@ -1,9 +0,0 @@
-package io.github.sceneview.components
-
-import io.github.sceneview.model.Model
-
-interface ModelChildComponent : Component {
-    val model: Model
-    val name: String? get() = model.getName(entity)
-    val extras: String? get() = model.getExtras(entity)
-}
