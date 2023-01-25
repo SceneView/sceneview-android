a14b6e9ac6d1977a635b5963b4e01f34ccf3a27e
Thomas Gorisse <thomas.gorisse@gmail.com>
2023-01-25 23:51:47 +0100

    Remove Compose material dependency

--- sceneview/src/main/java/io/github/sceneview/Scene.kt
+++ sceneview/src/main/java/io/github/sceneview/Scene.kt
@@ -2,10 +2,8 @@ package io.github.sceneview
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalInspectionMode
@@ -48,12 +46,5 @@ private fun ScenePreview(modifier: Modifier) {
     Box(
         modifier = modifier
             .background(Color.DarkGray)
-    ) {
-        Text(
-            modifier = Modifier
-                .align(Alignment.Center),
-            text = "SceneView",
-            color = Color.White
-        )
-    }
+    )
 }
