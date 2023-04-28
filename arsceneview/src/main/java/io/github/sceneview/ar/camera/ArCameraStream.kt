17bf31721bea69fce5c0fc024f5f072de724a172
Pawel Jobkiewicz <43797319+kiler222@users.noreply.github.com>
2023-04-28 12:39:06 +0200

    Fix for rendering semi-transparent models and plane renderer (#259)

--- arsceneview/src/main/java/io/github/sceneview/ar/camera/ArCameraStream.kt
+++ arsceneview/src/main/java/io/github/sceneview/ar/camera/ArCameraStream.kt
@@ -172,8 +172,6 @@ class ArCameraStream(
     val renderable: Renderable = RenderableManager.Builder(4)
         .castShadows(false)
         .receiveShadows(false)
-        // Use different channel from other ones
-        .channel(3)
         // Always draw the camera feed last to avoid overdraw
         .culling(false)
         .priority(priority)
