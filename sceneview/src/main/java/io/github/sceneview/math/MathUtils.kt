0785f1401e56a32df06c055611614c42f671f015
Thomas Gorisse <thomas.gorisse@gmail.com>
2023-06-28 00:11:40 +0200

    Added Matrix comparison (with possible delta)

--- sceneview/src/main/java/io/github/sceneview/math/MathUtils.kt
+++ sceneview/src/main/java/io/github/sceneview/math/MathUtils.kt
@@ -3,6 +3,7 @@ package io.github.sceneview.math
 import com.google.android.filament.Box
 import com.google.ar.sceneform.math.Vector3
 import dev.romainguy.kotlin.math.*
+import kotlin.math.absoluteValue
 
 typealias Position = Float3
 typealias Rotation = Float3
@@ -46,12 +47,16 @@ fun com.google.ar.sceneform.math.Quaternion.toNewQuaternion() = Quaternion(x, y,
 val Mat4.quaternion: Quaternion
     get() = rotation(this).toQuaternion()
 
+operator fun Mat4.times(v: Float3) = (this * Float4(v, 1f)).xyz
+
 fun Mat4.toColumnsFloatArray() = floatArrayOf(
     x.x, x.y, x.z, x.w,
     y.x, y.y, y.z, y.w,
     z.x, z.y, z.z, z.w,
     w.x, w.y, w.z, w.w
 )
+
+fun Mat4.toColumnsDoubleArray() = toColumnsFloatArray().map { it.toDouble() }.toDoubleArray()
 
 fun FloatArray.toTransform() = Transform(
     x = Float4(this[0], this[1], this[2], this[3]),
@@ -143,3 +148,67 @@ fun lookAt(eye: Position, target: Position): Mat4 {
 
 fun lookTowards(eye: Position, direction: Direction) =
     lookTowards(eye, direction, Direction(y = 1.0f)).toQuaternion()
+
+
+// TODO: Integrated in Kotlin-math 1.34.1 remove this when published.
+/////////////////////////////////////////////////////////////////////
+fun Float.compareTo(v: Float, delta: Float): Float = when {
+    equals(v, delta) -> 0.0f
+    else -> compareTo(v).toFloat()
+}
+
+fun Float.equals(v: Float, delta: Float) = (this - v).absoluteValue < delta
+
+fun Float2.compareTo(v: Float, delta: Float = 0.0f) = Float2(
+    x.compareTo(v, delta),
+    y.compareTo(v, delta)
+)
+
+fun Float2.equals(v: Float, delta: Float = 0.0f) = x.equals(v, delta) && y.equals(v, delta)
+
+fun Float2.compareTo(v: Float2, delta: Float = 0.0f) = Float2(
+    x.compareTo(v.x, delta),
+    y.compareTo(v.y, delta)
+)
+
+fun Float2.equals(v: Float2, delta: Float = 0.0f) = x.equals(v.x, delta) && y.equals(v.y, delta)
+
+fun Float3.compareTo(v: Float, delta: Float = 0.0f) = Float3(
+    xy.compareTo(v, delta),
+    z.compareTo(v, delta)
+)
+
+fun Float3.equals(v: Float, delta: Float = 0.0f) = xy.equals(v, delta) && z.equals(v, delta)
+
+fun Float3.compareTo(v: Float3, delta: Float = 0.0f) = Float3(
+    xy.compareTo(v.xy, delta),
+    z.compareTo(v.z, delta)
+)
+
+fun Float3.equals(v: Float3, delta: Float = 0.0f) =
+    xy.equals(v.xy, delta) && z.equals(v.z, delta)
+
+fun Float4.compareTo(v: Float, delta: Float = 0.0f) = Float4(
+    xyz.compareTo(v, delta),
+    w.compareTo(v, delta)
+)
+
+fun Float4.equals(v: Float, delta: Float = 0.0f) = xyz.equals(v, delta) && w.equals(v, delta)
+
+fun Float4.compareTo(v: Float4, delta: Float = 0.0f) = Float4(
+    xyz.compareTo(v.xyz, delta),
+    w.compareTo(v.w, delta)
+)
+
+fun Float4.equals(v: Float4, delta: Float = 0.0f) =
+    xyz.equals(v.xyz, delta) && w.equals(v.w, delta)
+
+fun Mat4.compareTo(m: Mat4, delta: Float = 0.0f) = Mat4(
+    x.compareTo(m.x, delta),
+    y.compareTo(m.y, delta),
+    z.compareTo(m.z, delta),
+    w.compareTo(m.w, delta)
+)
+
+fun Mat4.equals(m: Mat4, delta: Float = 0.0f) =
+    x.equals(m.x, delta) && y.equals(m.y, delta) && z.equals(m.z, delta) && w.equals(m.w, delta)
