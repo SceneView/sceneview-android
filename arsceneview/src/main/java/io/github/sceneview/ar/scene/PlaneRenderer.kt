1320d0eb3f91d68f33c3dc200e6366a538b82e50
Thomas Gorisse <thomas.gorisse@gmail.com>
2023-06-28 00:05:28 +0200

    Start migrating PlaneVisualizer to Kotlin

--- arsceneview/src/main/java/io/github/sceneview/ar/scene/PlaneRenderer.kt
+++ arsceneview/src/main/java/io/github/sceneview/ar/scene/PlaneRenderer.kt
@@ -313,3 +313,282 @@ class PlaneRenderer(val sceneView: ArSceneView) {
         private const val SPOTLIGHT_RADIUS = 0.5f
     }
 }
+
+// TODO: Finish move to Kotlin from there
+//class PlaneVisualizer(private val sceneView: SceneView, private val plane: Plane) :
+//    TransformProvider {
+//
+//    private val planeMatrix = Matrix()
+//    private var isPlaneAddedToScene = false
+//    private var isEnabled = false
+//    private var isShadowReceiver = false
+//    private var isVisible = false
+//
+//    @Entity
+//    private var planeRenderable: Int? = null
+//
+//    //    private var planeRenderable: ModelRenderable? = null
+//    private val planeRenderableInstance
+//        @EntityInstance get() = planeRenderable?.let {
+//            renderableManager.getInstance(it)
+//        }
+//
+//    //    private var planeRenderableInstance: RenderableInstance? = null
+//    private val vertices = ArrayList<Geometry.Vertex>()
+//    private val triangleIndices = ArrayList<Int>()
+//
+//    //    private val renderableDefinition: RenderableDefinition
+//    private val geometry: Geometry
+//    private var planeSubmesh: RenderableDefinition.Submesh? = null
+//    private var shadowSubmesh: RenderableDefinition.Submesh? = null
+//    fun setEnabled(enabled: Boolean) {
+//        if (isEnabled != enabled) {
+//            isEnabled = enabled
+//            updatePlane()
+//        }
+//    }
+//
+//    fun setShadowReceiver(shadowReceiver: Boolean) {
+//        if (isShadowReceiver != shadowReceiver) {
+//            isShadowReceiver = shadowReceiver
+//            updatePlane()
+//        }
+//    }
+//
+//    fun setVisible(visible: Boolean) {
+//        if (isVisible != visible) {
+//            isVisible = visible
+//            updatePlane()
+//        }
+//    }
+//
+//    init {
+//        geometry = Geometry.Builder(vertices = vertices).build()
+////        renderableDefinition = RenderableDefinition
+////            .builder()
+////            .setVertices(vertices)
+////            .build()
+//    }
+//
+//    fun setShadowMaterial(materialInstance: MaterialInstance?) {
+//        if (shadowSubmesh == null) {
+//            shadowSubmesh = RenderableDefinition.Submesh.builder()
+//                .setTriangleIndices(triangleIndices)
+//                .setMaterial(materialInstance)
+//                .build()
+//        } else {
+//            shadowSubmesh!!.material = materialInstance
+//        }
+//        if (planeRenderable != null) {
+//            updateRenderable()
+//        }
+//    }
+//
+//    fun setPlaneMaterial(materialInstance: MaterialInstance?) {
+//        if (planeSubmesh == null) {
+//            planeSubmesh = RenderableDefinition.Submesh.builder()
+//                .setTriangleIndices(triangleIndices)
+//                .setMaterial(materialInstance)
+//                .build()
+//        } else {
+//            planeSubmesh!!.material = materialInstance
+//        }
+//        if (planeRenderable != null) {
+//            updateRenderable()
+//        }
+//    }
+//
+//    override fun getTransformationMatrix(): Matrix {
+//        return planeMatrix
+//    }
+//
+//    fun updatePlane() {
+//        if (!isEnabled || !isVisible && !isShadowReceiver) {
+//            removePlaneFromScene()
+//            return
+//        }
+//        if (plane.trackingState != TrackingState.TRACKING) {
+//            removePlaneFromScene()
+//            return
+//        }
+//
+//        // Set the transformation matrix to the pose of the plane.
+//        plane.centerPose.toMatrix(planeMatrix.data, 0)
+//
+//        // Calculate the mesh for the plane.
+//        val success = updateRenderableDefinitionForPlane()
+//        if (!success) {
+//            removePlaneFromScene()
+//            return
+//        }
+//        updateRenderable()
+//        addPlaneToScene()
+//    }
+//
+//    fun updateRenderable() {
+//        val submeshes = geometry.submeshes
+////        val submeshes = renderableDefinition.getSubmeshes()
+//        submeshes.clear()
+//
+//        // the order of the meshes is important here, because we set the blendOrder based on
+//        // the index below.
+//        if (isVisible && planeSubmesh != null) {
+//            submeshes.add(planeSubmesh!!)
+//        }
+//        if (isShadowReceiver && shadowSubmesh != null) {
+//            submeshes.add(shadowSubmesh!!)
+//        }
+//        if (submeshes.isEmpty()) {
+//            removePlaneFromScene()
+//            return
+//        }
+//        if (planeRenderable == null) {
+//            try {
+//                planeRenderable = RenderableManager.Builder(geometry.submeshes.size)
+//                    .geometry(geometry)
+//                    .castShadows(false)
+//                    .build()
+////                planeRenderable = ModelRenderable.builder()
+////                    .setSource(renderableDefinition)
+////                    .build()
+////                    .get()
+////                planeRenderable.setShadowCaster(false)
+//                // Creating a Renderable is immediate when using RenderableDefinition.
+//            } catch (ex: InterruptedException) {
+//                throw AssertionError("Unable to create plane renderable.")
+//            } catch (ex: ExecutionException) {
+//                throw AssertionError("Unable to create plane renderable.")
+//            }
+////            planeRenderableInstance = planeRenderable?.renderableInstance
+////            planeRenderableInstance = planeRenderable.createInstance(this)
+//        } else {
+//            renderableManager.setGeometry()
+//            planeRenderable!!.updateFromDefinition(renderableDefinition)
+//        }
+//
+//        // The plane must always be drawn before the shadow, we use the blendOrder to enforce that.
+//        // this works because both sub-meshes will be sorted at the same distance from the camera
+//        // since they're part of the same renderable. The blendOrder, determines the sorting in
+//        // that situation.
+//        if (planeRenderableInstance != null && submeshes.size > 1) {
+//            planeRenderableInstance!!.setBlendOrderAt(0, 0) // plane
+//            planeRenderableInstance!!.setBlendOrderAt(1, 1) // shadow
+//        }
+//        planeRenderableInstance!!.prepareForDraw(sceneView)
+//
+//        val transformManager = transformManager
+//        val instance = transformManager.getInstance(planeRenderable)
+//        transformManager.setTransform(instance, planeRenderableInstance!!.worldModelMatrix.data)
+//    }
+//
+//    fun destroy() {
+//        removePlaneFromScene()
+//        planeRenderable?.destroyRenderable()
+////        if (planeRenderableInstance != null) {
+////            planeRenderableInstance!!.destroy()
+////        }
+//        planeRenderable = null
+//    }
+//
+//    private fun addPlaneToScene() {
+//        if (isPlaneAddedToScene || planeRenderableInstance == null) {
+//            return
+//        }
+//        planeRenderable?.let { sceneView.addEntity(it) }
+////        sceneView.addEntity(planeRenderableInstance!!.renderedEntity)
+//        isPlaneAddedToScene = true
+//    }
+//
+//    private fun removePlaneFromScene() {
+//        if (!isPlaneAddedToScene || planeRenderableInstance == null) {
+//            return
+//        }
+//        planeRenderable?.let { sceneView.removeEntity(it) }
+////        sceneView.removeEntity(planeRenderableInstance!!.renderedEntity)
+//        isPlaneAddedToScene = false
+//    }
+//
+//    private fun updateRenderableDefinitionForPlane(): Boolean {
+//        val boundary = plane.polygon ?: return false
+//        boundary.rewind()
+//        val boundaryVertices = boundary.limit() / 2
+//        if (boundaryVertices == 0) {
+//            return false
+//        }
+//        val numVertices: Int = boundaryVertices * VERTS_PER_BOUNDARY_VERT
+//        vertices.clear()
+//        vertices.ensureCapacity(numVertices)
+//        val numIndices = boundaryVertices * 6 + (boundaryVertices - 2) * 3
+//        triangleIndices.clear()
+//        triangleIndices.ensureCapacity(numIndices)
+//        // Up normal
+//        val normal = Direction(y = 1.0f)//Vector3.up()
+//
+//        // Copy the perimeter vertices into the vertex buffer and add in the y-coordinate.
+//        while (boundary.hasRemaining()) {
+//            val x = boundary.get()
+//            val z = boundary.get()
+//            vertices.add(
+//                Geometry.Vertex(position = Position(x, 0.0f, z), normal = normal)
+////                Vertex.builder().setPosition(Vector3(x, 0.0f, z)).setNormal(normal).build()
+//            )
+//        }
+//
+//        // Generate the interior vertices.
+//        boundary.rewind()
+//        while (boundary.hasRemaining()) {
+//            val x = boundary.get()
+//            val z = boundary.get()
+//            val magnitude = Math.hypot(x.toDouble(), z.toDouble()).toFloat()
+//            var scale: Float = 1.0f - FEATHER_SCALE
+//            if (magnitude != 0.0f) {
+//                scale = 1.0f - min(FEATHER_LENGTH / magnitude, FEATHER_SCALE)
+//            }
+//            vertices.add(
+//                Geometry.Vertex(
+//                    position = Position(x * scale, 1.0f, z * scale),
+//                    normal = normal
+//                )
+////                Vertex.builder()
+////                    .setPosition(Vector3(x * scale, 1.0f, z * scale))
+////                    .setNormal(normal)
+////                    .build()
+//            )
+//        }
+//        val firstOuterVertex = 0
+//        val firstInnerVertex = boundaryVertices.toShort().toInt()
+//
+//        // Generate triangle (4, 5, 6) and (4, 6, 7).
+//        for (i in 0 until boundaryVertices - 2) {
+//            triangleIndices.add(firstInnerVertex)
+//            triangleIndices.add(firstInnerVertex + i + 1)
+//            triangleIndices.add(firstInnerVertex + i + 2)
+//        }
+//
+//        // Generate triangle (0, 1, 4), (4, 1, 5), (5, 1, 2), (5, 2, 6), (6, 2, 3), (6, 3, 7)
+//        // (7, 3, 0), (7, 0, 4)
+//        for (i in 0 until boundaryVertices) {
+//            val outerVertex1 = firstOuterVertex + i
+//            val outerVertex2 = firstOuterVertex + (i + 1) % boundaryVertices
+//            val innerVertex1 = firstInnerVertex + i
+//            val innerVertex2 = firstInnerVertex + (i + 1) % boundaryVertices
+//            triangleIndices.add(outerVertex1)
+//            triangleIndices.add(outerVertex2)
+//            triangleIndices.add(innerVertex1)
+//            triangleIndices.add(innerVertex1)
+//            triangleIndices.add(outerVertex2)
+//            triangleIndices.add(innerVertex2)
+//        }
+//        return true
+//    }
+//
+//    companion object {
+//        private const val VERTS_PER_BOUNDARY_VERT = 2
+//
+//        // Feather distance 0.2 meters.
+//        private const val FEATHER_LENGTH = 0.2f
+//
+//        // Feather scale over the distance between plane center and vertices.
+//        private const val FEATHER_SCALE = 0.2f
+//    }
+//}
