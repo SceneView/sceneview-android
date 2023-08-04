26f38b2c634316ef076291a9f7d4a8170a39e2bb
Thomas Gorisse <thomas.gorisse@gmail.com>
2023-08-04 15:58:57 +0200

    HitTest default contextual values on ARFrame and ARSceneview

--- arsceneview/src/main/java/io/github/sceneview/ar/arcore/Camera.kt
+++ arsceneview/src/main/java/io/github/sceneview/ar/arcore/Camera.kt
@@ -6,8 +6,4 @@ import com.google.ar.core.TrackingState
 /**
  * Keep the screen unlocked while tracking, but allow it to lock when tracking stops.
  */
-val Camera.isTracking: Boolean
-    get() = when (this.trackingState!!) {
-        TrackingState.PAUSED, TrackingState.STOPPED -> false
-        TrackingState.TRACKING -> true
-    }
+val Camera.isTracking get() = trackingState == TrackingState.TRACKING
