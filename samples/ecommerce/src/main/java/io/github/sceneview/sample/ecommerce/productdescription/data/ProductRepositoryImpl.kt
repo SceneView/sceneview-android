ceda59eaea036b3117845ea642d9ab74e74ce7c8
vliang-mbp14 <vliang621@gmail.com>
2023-08-11 18:03:00 -0700

    small changes

--- samples/ecommerce/src/main/java/io/github/sceneview/sample/ecommerce/productdescription/data/ProductRepositoryImpl.kt
+++ samples/ecommerce/src/main/java/io/github/sceneview/sample/ecommerce/productdescription/data/ProductRepositoryImpl.kt
@@ -9,7 +9,6 @@ class ProductRepositoryImpl : ProductRepository {
                 "https://www.longchamp.com/dw/image/v2/BCVX_PRD/on/demandware.static/-/Sites-LC-master-catalog/default/dwa66395c1/images/DIS/10095HQS504_0.png?sw=800&sh=800&sm=fit",
                 "https://www.longchamp.com/dw/image/v2/BCVX_PRD/on/demandware.static/-/Sites-LC-master-catalog/default/dw63a629da/images/DIS/10095HQS504_1.png?sw=800&sh=800&sm=fit"
             ),
-            assetUrl = "asdf",
             description = "Detachable shoulder strap, Bamboo clasp, Feet on the bottom, 1 zipped pocket and 1 flat pocket inside",
             title = "ROSEAU S HANDBAG",
             color = "Cognac - Leather",
