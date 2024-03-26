575d95c99d7163c7976bf3beef5047ad25391a43
Himanshu Walia <himanshu.walia@gmail.com>
2024-03-26 23:58:14 +0000

    Update model viewer sample to use version 2.0.2 (#405)
    * update model viewer to use version 2.0.2
    
    * Fix env set indirect light and skybox from HDR
    
    ---------
    
    Co-authored-by: Himanshu Walia <Himanshu.Walia@dyson.com>
    Co-authored-by: Thomas Gorisse <thomas.gorisse@gmail.com>

--- samples/model-viewer/src/main/java/io/github/sceneview/sample/modelviewer/MainFragment.kt
+++ samples/model-viewer/src/main/java/io/github/sceneview/sample/modelviewer/MainFragment.kt
@@ -6,59 +6,42 @@ import androidx.core.view.isGone
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import io.github.sceneview.SceneView
-import io.github.sceneview.loaders.loadHdrIndirectLight
-import io.github.sceneview.loaders.loadHdrSkybox
 import io.github.sceneview.math.Position
 import io.github.sceneview.math.Rotation
-import io.github.sceneview.nodes.ModelNode
-import io.github.sceneview.nodes.ViewNode
+import io.github.sceneview.math.Scale
+import io.github.sceneview.node.ModelNode
+import kotlinx.coroutines.launch
 
 class MainFragment : Fragment(R.layout.fragment_main) {
 
-    lateinit var sceneView: SceneView
-    lateinit var loadingView: View
+    private lateinit var sceneView: SceneView
+    private lateinit var loadingView: View
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        sceneView = view.findViewById<SceneView>(R.id.sceneView).apply {
-            setLifecycle(lifecycle)
-        }
+        sceneView = view.findViewById(R.id.sceneView)
         loadingView = view.findViewById(R.id.loadingView)
 
-        lifecycleScope.launchWhenCreated {
+        viewLifecycleOwner.lifecycleScope.launch {
             val hdrFile = "environments/studio_small_09_2k.hdr"
-            sceneView.loadHdrIndirectLight(hdrFile, specularFilter = true) {
-                intensity(30_000f)
-            }
-            sceneView.loadHdrSkybox(hdrFile) {
-                intensity(50_000f)
-            }
 
-            val model = sceneView.modelLoader.loadModel("models/MaterialSuite.glb")!!
-            val modelNode = ModelNode(sceneView, model).apply {
-                transform(
-                    position = Position(z = -4.0f),
-                    rotation = Rotation(x = 15.0f)
-                )
-                scaleToUnitsCube(2.0f)
-                // TODO: Fix centerOrigin
-                //  centerOrigin(Position(x=-1.0f, y=-1.0f))
-                playAnimation()
+            sceneView.environmentLoader.loadHDREnvironment(hdrFile).apply {
+                sceneView.indirectLight = this?.indirectLight
+                sceneView.skybox = this?.skybox
             }
+            sceneView.cameraNode.apply {
+                position = Position(z = 4.0f)
+            }
+            val modelFile = "models/MaterialSuite.glb"
+            val modelInstance = sceneView.modelLoader.createModelInstance(modelFile)
+
+            val modelNode = ModelNode(
+                modelInstance = modelInstance,
+                scaleToUnits = 2.0f,
+            )
+            modelNode.scale = Scale(0.05f)
             sceneView.addChildNode(modelNode)
-
-            val viewNode = ViewNode(
-                sceneView = sceneView,
-                viewResourceId = R.layout.view_node_layout
-            ).apply {
-                transform(
-                    position = Position(z = -4f),
-                    rotation = Rotation()
-                )
-            }
-            sceneView.addChildNode(viewNode)
-
             loadingView.isGone = true
         }
     }
