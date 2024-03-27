e5778fdd8b7f80d73c50be0313137d5a815a6b8f
Thomas Gorisse <thomas.gorisse@gmail.com>
2024-03-27 01:04:03 +0100

    Delete `sceneview_2_0_0` folder

--- sceneview_2_0_0/src/main/java/io/github/sceneview/components/Component.kt
+++
@@ -1,9 +0,0 @@
-package io.github.sceneview.components
-
-import com.google.android.filament.Engine
-import io.github.sceneview.Entity
-
-interface Component {
-    val engine: Engine
-    val entity: Entity
-}
