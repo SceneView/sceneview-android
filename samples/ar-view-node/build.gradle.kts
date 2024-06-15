f92a05ea3d6dda74cf3aa0a4f6273768ee82fbc3
Robert Gregat <r.gregat@netpipe.io>
2024-06-15 09:22:24 +0200

    chore: Update dependencies
    * androidx.core:core-ktx: 1.12.0 -> 1.13.1
    * androidx.appcompat:appcompat: 1.6.1 -> 1.7.0
    * com.google.android.material:material: 1.11.0 -> 1.12.0

--- samples/ar-view-node/build.gradle.kts
+++ samples/ar-view-node/build.gradle.kts
@@ -45,9 +45,10 @@ dependencies {
     //debugImplementation(project(":arsceneview"))
     implementation(project(":arsceneview"))
     implementation(project(":samples:common"))
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
+
+    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.github.bumptech.glide:glide:4.16.0")
     testImplementation("junit:junit:4.13.2")
