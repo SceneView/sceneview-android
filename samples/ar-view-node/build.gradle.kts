f7cf29378989f02075f8c1f348981553771b7acb
Robert Gregat <r.gregat@netpipe.io>
2024-06-15 09:20:22 +0200

    chore: Remove default ic_launcher resource and include SceneView ic_launcher res

--- samples/ar-view-node/build.gradle.kts
+++ samples/ar-view-node/build.gradle.kts
@@ -44,6 +44,7 @@ dependencies {
     //releaseImplementation("io.github.sceneview:arsceneview:2.0.3")
     //debugImplementation(project(":arsceneview"))
     implementation(project(":arsceneview"))
+    implementation(project(":samples:common"))
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
