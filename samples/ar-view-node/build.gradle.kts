fb32f95f017b1c6866554d35e5e5a5766e7a46d3
Robert Gregat <r.gregat@netpipe.io>
2024-06-15 09:24:14 +0200

    chore: Use SceneView debug or release implementation, depending on the set build type

--- samples/ar-view-node/build.gradle.kts
+++ samples/ar-view-node/build.gradle.kts
@@ -41,16 +41,17 @@ android {
 }
 
 dependencies {
-    //releaseImplementation("io.github.sceneview:arsceneview:2.0.3")
-    //debugImplementation(project(":arsceneview"))
-    implementation(project(":arsceneview"))
+    releaseImplementation("io.github.sceneview:arsceneview:2.2.1")
+    debugImplementation(project(":arsceneview"))
     implementation(project(":samples:common"))
 
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+
     implementation("com.github.bumptech.glide:glide:4.16.0")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
