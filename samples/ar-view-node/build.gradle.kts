22f00e8bd2b215c5cd37e33e11720849a5990c0e
Robert Gregat <r.gregat@netpipe.io>
2024-06-19 08:34:46 +0200

    chore: Remove unnecessary dependencies.

--- samples/ar-view-node/build.gradle.kts
+++ samples/ar-view-node/build.gradle.kts
@@ -51,8 +51,4 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 
     implementation("com.github.bumptech.glide:glide:4.16.0")
-
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
