2bacfd649b15f67e0b2c5212281ed1f62faf561d
Bencze Kalman <kalman.bencze@ingka.ikea.com>
2025-11-10 11:33:19 +0200

    Upgraded gradle build config to use KTS and misc improvements (#642)

---
+++ samples/ar-augmented-image/build.gradle.kts
@@ -0,0 +1,40 @@
+import org.gradle.kotlin.dsl.debugImplementation
+
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+}
+
+android {
+    namespace = "io.github.sceneview.sample.araugmentedimage"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "io.github.sceneview.sample.araugmentedimage"
+        minSdk = 24
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0.0"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
+    }
+    androidResources {
+        noCompress.add("filamat")
+        noCompress.add("ktx")
+    }
+}
+
+dependencies {
+    implementation(projects.samples.common)
+
+    // ExoPlayer
+    implementation(libs.androidx.media3.exoplayer)
+    implementation(libs.androidx.media3.ui)
+
+    // ARSceneview
+    releaseImplementation(libs.arsceneview)
+    debugImplementation(projects.arsceneview)
+}
